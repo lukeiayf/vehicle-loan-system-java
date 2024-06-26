ce0662faf97e6ca02b993fee24bcac1d795d8218
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-06-26 15:25:43 -0300

    init classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,32 @@
 package application;
 
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Enter loan data: ");
+        System.out.println("Car model: ");
+        String carModel = sc.nextLine();
+        System.out.println("Start time (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Finish time (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+
+        sc.close();
     }
 }
